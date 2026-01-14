c7a4d819e2e20c253f19c0d8af66f5ec173c2de4
Omar Abi Farraj <omar.7tech@gmail.com>
2026-01-14 13:50:33 +0200

    first commit

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,17 @@
+import { treaty } from '@elysiajs/eden'
+import { Elysia, t } from 'elysia'
+
+export const app = new Elysia({ prefix: '/api' }) 
+	.get('/', 'Hello Nextjs')
+	.post(
+		'/user',
+		({ body }) => body,
+		{
+			body: t.Object({
+				name: t.String()
+			})
+		}
+	)
+
+export const GET = app.fetch
+export const POST = app.fetch
